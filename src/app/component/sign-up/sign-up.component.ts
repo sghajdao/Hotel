b2588b39e1e8f54181956ec644deaecbb7095f47
sghajdao <saadghajdaoui@gmail.com>
2022-12-22 14:47:49 +0100

    add error message in sign-up formular

--- src/app/component/sign-up/sign-up.component.ts
+++ src/app/component/sign-up/sign-up.component.ts
@@ -12,6 +12,8 @@ import { Account } from '../../model/data.model';
 export class SignUpComponent implements OnInit {
 
   signUpData:FormGroup
+  validators:number[] = [1, 1, 1, 1]
+
   constructor(private fb:FormBuilder, private service:RoomsServices, private router:Router) {
     this.signUpData = this.fb.group({
       username:['', Validators.required],
@@ -25,9 +27,20 @@ export class SignUpComponent implements OnInit {
   }
 
   sign_up(){
-    if(this.signUpData.value.password === this.signUpData.value.password2){
+    if(this.signUpData.value.password === this.signUpData.value.password2 && this.signUpData.value.username && this.signUpData.value.email && this.signUpData.value.password && this.signUpData.value.password2){
       let account = this.signUpData.value;
       this.service.saveAccount(account).subscribe(data=>{alert("Success saving!"); this.router.navigate(['/'])})
     }
+    else{
+      if (!this.signUpData.value.username){this.validators[0] = 0}
+      else{this.validators[0] = 1}
+      if (!this.signUpData.value.email){this.validators[1] = 0}
+      else{this.validators[1] = 1}
+      if (!this.signUpData.value.password){this.validators[2] = 0}
+      else{this.validators[2] = 1}
+      if (!this.signUpData.value.password2){this.validators[3] = 0}
+      else{this.validators[3] = 1}
+    }
+    if(this.signUpData.value.password != this.signUpData.value.password2){this.validators[3] = 2}
   }
 }
